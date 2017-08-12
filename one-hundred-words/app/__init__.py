f2b7fd10ff88d620dadd65babbc8fa4478090d4c
Robert Rees <shudderfix@gmail.com>
2017-08-12 23:58:59 +0100

    Creates a new app to test posting

---
+++ one-hundred-words/app/__init__.py
@@ -0,0 +1,13 @@
+
+from flask import Flask
+
+import handlers
+
+app = Flask(__name__)
+
+routes = [
+	('/', 'index', handlers.index, ['GET', 'POST']),
+]
+
+for path, endpoint, handler, methods in routes:
+	app.add_url_rule(path, endpoint, handler, methods=methods)
